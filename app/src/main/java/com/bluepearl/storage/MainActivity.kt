2382fd3f83a69c8ca7737762c07ef3237b2cc92a
Himanshu Pandit <Lenovo@DESKTOP-L0U0LE4>
2024-10-22 13:21:51 +0530

    loader & alert added

--- app/src/main/java/com/bluepearl/storage/MainActivity.kt
+++ app/src/main/java/com/bluepearl/storage/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.bluepearl.storage
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,12 +14,25 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,25 +62,124 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
     val context  = LocalContext.current
+    var showLoader by remember { mutableStateOf(false) }
+    var showAlert by remember { mutableStateOf(false) }
+    var message by remember { mutableStateOf("") }
+    var fileUri by remember { mutableStateOf<Uri?>(null) }
     val pdfUrl = "https://research.nhm.org/pdfs/10840/10840.pdf"
 
     Column (
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ){
 
+
+        if(showLoader){
+            CircularProgressIndicator(
+                modifier = Modifier.width(64.dp),
+                color = MaterialTheme.colorScheme.secondary,
+                trackColor = MaterialTheme.colorScheme.surfaceVariant,
+            )
+        }
+
+
         Button(
             onClick = {
+                showLoader = true
                 val downloader = Downloader()
-                val downloadId = downloader.downloadPdfPublic(context, pdfUrl, )
-//                val downloadId = downloader.downloadPdfPrivate(context, pdfUrl)
+                val downloadId = downloader.downloadPdfPublic(
+                    context,
+                    pdfUrl,
+                    onSuccess = { uri: Uri ->
+                        showLoader = false
+                        showAlert = true
+                        fileUri = uri
+                        message = "Your download has successfully completed."
+                    },
+                    onFailure = {
+                        showLoader = false
+                        showAlert = true
+                        message = "Download failed."
+                    }
+                )
                 Log.d("downloadId", downloadId.toString())
             },
             modifier = Modifier.padding(32.dp)
         ) {
             Text("Download Pdf")
         }
+
+        if (showAlert){
+            ShowAlert(
+                onDismissRequest = {
+                    showAlert = false
+                },
+                onConfirmation = {
+                    showAlert = false
+
+                     //Create an intent to view the downloaded PDF
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.setDataAndType(fileUri, "application/pdf")
+                    intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_GRANT_READ_URI_PERMISSION
+
+                    // Check if there is an app that can handle the intent
+                    try {
+                        context.startActivity(intent)
+                    }catch (e: Exception){
+                        Log.d("Downloader", "Failed to open PDF: ${e.message}")
+                        Toast.makeText(context, "Error opening PDF file.", Toast.LENGTH_SHORT).show()
+                    }
+                },
+                "Report download",
+                "Your download has successfully completed.",
+                Icons.Default.Done
+            )
+        }
+
     }
+}
+
+@Composable
+fun ShowAlert(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+) {
+    AlertDialog(
+        icon = {
+            Icon(icon, contentDescription = "Download Icon")
+        },
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            Text(text = dialogText)
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text("Okay")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                }
+            ) {
+                Text("Open")
+            }
+        }
+    )
 }
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
--- app/src/main/java/com/bluepearl/storage/util/Downloader.kt
+++ app/src/main/java/com/bluepearl/storage/util/Downloader.kt
@@ -1,7 +1,6 @@
 package com.bluepearl.storage.util
 
 import android.annotation.SuppressLint
-import android.app.AlertDialog
 import android.app.DownloadManager
 import android.content.BroadcastReceiver
 import android.content.Context
@@ -11,16 +10,21 @@ import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.util.Log
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 
 class Downloader {
 
-    var downloadId: Long = -1  // Variable to store the ID of the download request
+    private var downloadId: Long = -1  // Variable to store the ID of the download request
+    private var receiver: BroadcastReceiver? = null
 
     // Method to initiate a PDF download to the public directory
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    fun downloadPdfPublic(context: Context, url: String) {
+    fun downloadPdfPublic(
+        context: Context,
+        url: String,
+        onSuccess: (Uri) -> Unit,
+        onFailure: () -> Unit
+    ) {
 
         Log.d("Downloader", "downloadPdf called with URL: $url")
 
@@ -41,120 +45,79 @@ class Downloader {
             .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE or DownloadManager.Request.NETWORK_WIFI)  // Allow both mobile data and WiFi
             .setAllowedOverMetered(true)  // Allow downloads over metered networks
             .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)  // Show a notification when the download is complete
-            .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "$eLabFolder/$fileName")  // Specify the destination for the downloaded file
-            //.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, "$eLabFolder/$fileName")  // Specify the destination for the downloaded file
+            //.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "$eLabFolder/$fileName")  // Specify the destination for the downloaded file
+            .setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, "$eLabFolder/$fileName")  // Specify the destination for the internal app's downloaded file
 
         // Enqueue the download request and store the download ID
         downloadId = downloadManager.enqueue(request)
 
         Log.d("Downloader", "Download enqueued with ID: $downloadId")
 
+        // Register the broadcast receiver
+        receiver = downloadReceiver(onSuccess, onFailure)
         // Register a broadcast receiver to listen for download completion
         val filter = IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)
-        context.registerReceiver(downloadReceiver, filter, Context.RECEIVER_EXPORTED)
+        context.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED)
     }
 
     // BroadcastReceiver to handle the download completion event
-    private val downloadReceiver = object : BroadcastReceiver() {
-        @SuppressLint("Range")
-        override fun onReceive(context: Context?, intent: Intent?) {
+    private fun downloadReceiver(onSuccess: (Uri) -> Unit, onFailure: () -> Unit): BroadcastReceiver {
+        return object : BroadcastReceiver() {
 
-            Log.d("Downloader", "onReceive called")
+            @SuppressLint("Range")
+            override fun onReceive(context: Context?, intent: Intent?) {
 
-            // Retrieve the ID of the completed download from the intent
-            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+                Log.d("Downloader", "onReceive called")
 
-            Log.d("DownloadReceiver", "onReceive called with download ID: $id")
+                // Retrieve the ID of the completed download from the intent
+                val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
 
-            // Check if the download ID matches the one initiated by the app
-            if (id == downloadId) {
-                Log.d("DownloadReceiver", "Download ID matches, proceeding with query")
+                Log.d("DownloadReceiver", "onReceive called with download ID: $id")
 
-                // Query the DownloadManager for details about the download
-                val downloadManager = context?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-                val query = DownloadManager.Query().setFilterById(downloadId)
-                val cursor = downloadManager.query(query)
+                // Check if the download ID matches the one initiated by the app
+                if (id == downloadId) {
+                    Log.d("DownloadReceiver", "Download ID matches, proceeding with query")
 
-                // If a matching download is found, check its status
-                if (cursor.moveToFirst()) {
-                    val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
-                    Log.d("DownloadReceiver", "Download status: $status")
+                    // Query the DownloadManager for details about the download
+                    val downloadManager = context?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+                    val query = DownloadManager.Query().setFilterById(downloadId)
+                    val cursor = downloadManager.query(query)
 
-                    // Handle the download status: success or failure
-                    when (status) {
-                        DownloadManager.STATUS_SUCCESSFUL -> {
-                            Log.d("DownloadReceiver2", "Download completed successfully")
+                    // If a matching download is found, check its status
+                    if (cursor.moveToFirst()) {
+                        val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                        Log.d("DownloadReceiver", "Download status: $status")
 
-                            // Get the URI of the downloaded file
-                            val uri = downloadManager.getUriForDownloadedFile(downloadId)
+                        // Handle the download status: success or failure
+                        when (status) {
+                            DownloadManager.STATUS_SUCCESSFUL -> {
+                                Log.d("DownloadReceiver2", "Download completed successfully")
 
-                            // Show an alert dialog with options to open the file or dismiss
-                            showAlert(context, "Download Completed", "Your download has successfully completed.", uri)
+                                // Get the URI of the downloaded file
+                                val uri = downloadManager.getUriForDownloadedFile(downloadId)
+
+                                // Call the success callback
+                                onSuccess(uri)
+                            }
+                            DownloadManager.STATUS_FAILED -> {
+                                Log.d("DownloadReceiver2", "Download failed")
+
+                                onFailure()
+                            }
+                            else -> {
+                                Log.d("DownloadReceiver2", "Unknown download status: $status")
+
+                                onFailure()
+                            }
                         }
-                        DownloadManager.STATUS_FAILED -> {
-                            Log.d("DownloadReceiver2", "Download failed")
-
-                            // Show an alert dialog indicating download failure
-                            showAlert(context, "Download Failed", "Your download has failed.", null)
-                        }
-                        DownloadManager.STATUS_PAUSED -> {
-                            Log.d("DownloadReceiver2", "Download paused")
-
-                            // Show an alert dialog indicating download failure
-                            showAlert(context, "Download Paused", "Your download has been paused. Please check your internet connection.", null)
-                        }
-                        DownloadManager.STATUS_RUNNING -> {
-                            Log.d("DownloadReceiver2", "Download is running")
-
-                            // Show an alert dialog indicating download failure
-                            showAlert(context, "Download is Running", "Your download has been running.", null)
-                        }
-                        else -> {
-                            Log.d("DownloadReceiver2", "Unknown download status: $status")
-
-                            // Handle any unknown status
-                            showAlert(context, "Download Failed", "Something went wrong, Please try again later.", null)
-                        }
+                    } else {
+                        Log.d("DownloadReceiver", "Cursor is empty, no matching download found")
                     }
+                    cursor.close()  // Close the cursor to avoid memory leaks
                 } else {
-                    Log.d("DownloadReceiver", "Cursor is empty, no matching download found")
-                }
-                cursor.close()  // Close the cursor to avoid memory leaks
-            } else {
-                Log.d("DownloadReceiver", "Download ID does not match")
-            }
-        }
-    }
-
-    // Function to show an alert dialog with two buttons: "Okay" and "Open" (if a file URI is available)
-    private fun showAlert(context: Context, title: String, message: String, fileUri: Uri?) {
-        val builder = AlertDialog.Builder(context)
-        builder.setTitle(title)  // Set the title of the dialog
-        builder.setMessage(message)  // Set the message of the dialog
-
-        // Add a "Okay" button to dismiss the dialog
-        builder.setPositiveButton("Okay") { dialog, _ -> dialog.dismiss() }
-
-        // If a valid file URI is provided, add an "Open" button to open the downloaded file
-        if (fileUri != null) {
-            builder.setNegativeButton("Open") { dialog, _ ->
-
-                // Create an intent to view the downloaded PDF
-                val intent = Intent(Intent.ACTION_VIEW)
-                intent.setDataAndType(fileUri, "application/pdf")
-                intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_GRANT_READ_URI_PERMISSION
-
-                // Check if there is an app that can handle the intent
-                try {
-                    context.startActivity(intent)
-                }catch (e: Exception){
-                    Log.d("Downloader", "Failed to open PDF: ${e.message}")
-                    Toast.makeText(context, "Error opening PDF file.", Toast.LENGTH_SHORT).show()
+                    Log.d("DownloadReceiver", "Download ID does not match")
                 }
             }
         }
-
-        // Show the dialog
-        builder.show()
     }
 }
